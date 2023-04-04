2d3a9d6d3fd2c1dca10a8456e8fe5cd22ed02be2
moriharanariki <nariki@gmail.com>
2023-04-04 10:58:52 +0900

    first commit

---
+++ src/main/java/com/example/demo/MercariBatchMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercariBatchMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercariBatchMybatisApplication.class, args);
+	}
+
+}
